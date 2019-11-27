766dbeb6ee3153d98ff37f1996825632243922e4
Linh <46288798+s272936@users.noreply.github.com>
2019-11-27 13:10:16 -0500

    update

---
+++ MinValueLinhHo/MinValueLinhHo/MinValueForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace MinValueLinhHo
+{
+    partial class MinValueForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(142, 330);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(51, 20);
+            this.lblAnswer.TabIndex = 10;
+            this.lblAnswer.Text = "label1";
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.ItemHeight = 20;
+            this.lstNumbers.Location = new System.Drawing.Point(37, 99);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(274, 204);
+            this.lstNumbers.TabIndex = 9;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(121, 43);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(91, 28);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // MinValueForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 392);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Name = "MinValueForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ MinValueLinhHo/MinValueLinhHo/MinValueForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinValueLinhHo
+{
+    public partial class MinValueForm : Form
+    {
+        public MinValueForm()
+        {
+            InitializeComponent();
+            // hide label
+            lblAnswer.Hide();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            // declare local variables and constants
+            const int MAX_ARRAY_SIZE = 10;
+            const int MAX_RANDOM_NUMBER = 500;
+            int[] arrayOfMin = new int[MAX_ARRAY_SIZE];
+            int counter, randomNumber, minValue;
+            Random randomNumberGenerator = new Random();
+            // Clear list box
+            this.lstNumbers.Items.Clear();
+
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                // generate a new random number between 1 and 500
+                randomNumber = randomNumberGenerator.Next(1, MAX_RANDOM_NUMBER + 1);
+
+                // assign the random number to the cell at position counter in the array
+                arrayOfMin[counter] = randomNumber;
+
+                // Display list
+                this.lstNumbers.Items.Add(randomNumber);
+
+                // refresh the form to display the new item in the list box
+                this.Refresh();
+            }
+
+            // get the min value in the array
+            minValue = GetMinValue(arrayOfMin);
+
+            // show label
+            lblAnswer.Show();
+
+            // display the min value in the label 
+            this.lblAnswer.Text = "The max value is: " + minValue;
+        }
+    }
+}
