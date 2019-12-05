9dcec63c743d771b5e323da2b25d359254878d06
Linh <s272936@ocsb.local>
2019-12-05 12:46:04 -0500

    update

--- FileRWLinhH/FileRWLinhH/FileReadWrite.cs
+++ FileRWLinhH/FileRWLinhH/FileReadWrite.cs
@@ -16,7 +16,7 @@ namespace FileRWLinhH
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FileReadWriteForm());
         }
     }
 }
