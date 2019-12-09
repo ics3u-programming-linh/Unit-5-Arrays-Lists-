9e66dc7711a16058d54291f2b7e62382442135ae
Linh <46288798+s272936@users.noreply.github.com>
2019-12-08 20:32:56 -0500

    Github

--- DealCardLinhHoo/DealCardLinhHoo/DealCardForm.cs
+++ DealCardLinhHoo/DealCardLinhHoo/DealCardForm.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+ * Created by: Linh Ho
+ * Created on: December 8th, 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #37 - Deal Card
+ * This program...generates 52 cards without repeating...
+ * when it runs out of cards, it will display a message box
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -29,73 +38,73 @@ namespace DealCardLinhHoo
         private void NewDeck()
         {
             // Make cards face down
-            CardsDown(); 
+            CardsDown();
 
             // Populate list with 52 cards
-            listCardImages.Insert(0, Properties.Resources._2C);
-            listCardImages.Insert(1, Properties.Resources._2D);
-            listCardImages.Insert(2, Properties.Resources._2H);
-            listCardImages.Insert(3, Properties.Resources._2S);
+            listCardImages.Add(Properties.Resources._2C);
+            listCardImages.Add(Properties.Resources._2D);
+            listCardImages.Add(Properties.Resources._2H);
+            listCardImages.Add(Properties.Resources._2S);
 
-            listCardImages.Insert(4, Properties.Resources._3C);
-            listCardImages.Insert(5, Properties.Resources._3D);
-            listCardImages.Insert(6, Properties.Resources._3H);
-            listCardImages.Insert(7, Properties.Resources._3S);
+            listCardImages.Add(Properties.Resources._3C);
+            listCardImages.Add(Properties.Resources._3D);
+            listCardImages.Add(Properties.Resources._3H);
+            listCardImages.Add(Properties.Resources._3S);
 
-            listCardImages.Insert(8, Properties.Resources._4C);
-            listCardImages.Insert(9, Properties.Resources._4D);
-            listCardImages.Insert(10, Properties.Resources._4H);
-            listCardImages.Insert(11, Properties.Resources._4S);
+            listCardImages.Add(Properties.Resources._4C);
+            listCardImages.Add(Properties.Resources._4D);
+            listCardImages.Add(Properties.Resources._4H);
+            listCardImages.Add(Properties.Resources._4S);
 
-            listCardImages.Insert(12, Properties.Resources._5C);
-            listCardImages.Insert(13, Properties.Resources._5D);
-            listCardImages.Insert(14, Properties.Resources._5H);
-            listCardImages.Insert(15, Properties.Resources._5S);
+            listCardImages.Add(Properties.Resources._5C);
+            listCardImages.Add(Properties.Resources._5D);
+            listCardImages.Add(Properties.Resources._5H);
+            listCardImages.Add(Properties.Resources._5S);
 
-            listCardImages.Insert(16, Properties.Resources._6C);
-            listCardImages.Insert(17, Properties.Resources._6D);
-            listCardImages.Insert(18, Properties.Resources._6H);
-            listCardImages.Insert(19, Properties.Resources._6S);
+            listCardImages.Add(Properties.Resources._6C);
+            listCardImages.Add(Properties.Resources._6D);
+            listCardImages.Add(Properties.Resources._6H);
+            listCardImages.Add(Properties.Resources._6S);
 
-            listCardImages.Insert(20, Properties.Resources._7C);
-            listCardImages.Insert(21, Properties.Resources._7D);
-            listCardImages.Insert(22, Properties.Resources._7H);
-            listCardImages.Insert(23, Properties.Resources._7S);
+            listCardImages.Add(Properties.Resources._7C);
+            listCardImages.Add(Properties.Resources._7D);
+            listCardImages.Add(Properties.Resources._7H);
+            listCardImages.Add(Properties.Resources._7S);
 
-            listCardImages.Insert(24, Properties.Resources._8C);
-            listCardImages.Insert(25, Properties.Resources._8D);
-            listCardImages.Insert(26, Properties.Resources._8H);
-            listCardImages.Insert(27, Properties.Resources._8S);
+            listCardImages.Add(Properties.Resources._8C);
+            listCardImages.Add(Properties.Resources._8D);
+            listCardImages.Add(Properties.Resources._8H);
+            listCardImages.Add(Properties.Resources._8S);
 
-            listCardImages.Insert(28, Properties.Resources._9C);
-            listCardImages.Insert(29, Properties.Resources._9D);
-            listCardImages.Insert(30, Properties.Resources._9H);
-            listCardImages.Insert(31, Properties.Resources._9S);
+            listCardImages.Add(Properties.Resources._9C);
+            listCardImages.Add(Properties.Resources._9D);
+            listCardImages.Add(Properties.Resources._9H);
+            listCardImages.Add(Properties.Resources._9S);
 
-            listCardImages.Insert(32, Properties.Resources._10C);
-            listCardImages.Insert(33, Properties.Resources._10D);
-            listCardImages.Insert(34, Properties.Resources._10H);
-            listCardImages.Insert(35, Properties.Resources._10S);
+            listCardImages.Add(Properties.Resources._10C);
+            listCardImages.Add(Properties.Resources._10D);
+            listCardImages.Add(Properties.Resources._10H);
+            listCardImages.Add(Properties.Resources._10S);
 
-            listCardImages.Insert(36, Properties.Resources.JC);
-            listCardImages.Insert(37, Properties.Resources.JD);
-            listCardImages.Insert(38, Properties.Resources.JH);
-            listCardImages.Insert(39, Properties.Resources.JS);
+            listCardImages.Add(Properties.Resources.JC);
+            listCardImages.Add(Properties.Resources.JD);
+            listCardImages.Add(Properties.Resources.JH);
+            listCardImages.Add(Properties.Resources.JS);
 
-            listCardImages.Insert(40, Properties.Resources.KC);
-            listCardImages.Insert(41, Properties.Resources.KD);
-            listCardImages.Insert(42, Properties.Resources.KH);
-            listCardImages.Insert(43, Properties.Resources.KS);
+            listCardImages.Add(Properties.Resources.KC);
+            listCardImages.Add(Properties.Resources.KD);
+            listCardImages.Add(Properties.Resources.KH);
+            listCardImages.Add(Properties.Resources.KS);
 
-            listCardImages.Insert(44, Properties.Resources.QC);
-            listCardImages.Insert(45, Properties.Resources.QD);
-            listCardImages.Insert(46, Properties.Resources.QH);
-            listCardImages.Insert(47, Properties.Resources.QS);
+            listCardImages.Add(Properties.Resources.QC);
+            listCardImages.Add(Properties.Resources.QD);
+            listCardImages.Add(Properties.Resources.QH);
+            listCardImages.Add(Properties.Resources.QS);
 
-            listCardImages.Insert(48, Properties.Resources.AC);
-            listCardImages.Insert(49, Properties.Resources.AD);
-            listCardImages.Insert(50, Properties.Resources.AH);
-            listCardImages.Insert(51, Properties.Resources.AS);
+            listCardImages.Add(Properties.Resources.AC);
+            listCardImages.Add(Properties.Resources.AD);
+            listCardImages.Add(Properties.Resources.AH);
+            listCardImages.Add(Properties.Resources.AS);
 
         }
 
@@ -132,14 +141,13 @@ namespace DealCardLinhHoo
                 // show message box if cards run out
                 MessageBox.Show("Ran out of cards. Deck is reshuffling.");
 
-                // clear images
+                // clear image
                 this.listCardImages.Clear();
 
                 // Call New Deck and Cards Down
                 CardsDown();
                 NewDeck();
                 
-
             }
             else
             {
